b3616f0d7a52c35a0f1b42176ce78043af76bc59
KaGet27 <paul.guicharrousse@hotmail.fr>
2020-11-08 21:42:23 +0100

    [FEAT] Fin API CRUD

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ mongoose.connect('mongodb+srv://KaGet27:Bnp2849.@cluster0.m5tzg.mongodb.net/test
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 const app = express();
 
-//Gestion du CORS = Cross Origin Resource Sharing quand back et front n'on pas la même origine
+//Gestion du CORS = Cross Origin Resource Sharing quand back et front n'ont pas la même origine
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.post('/api/stuff', (req, res, next) => {
     delete req.body._id;
     const thing = new Thing({
-        // ... permet de mapper les objets du form client directement dans notre model
+        // ... permet de mapper les objets du form client directement dans notre model things
        ...req.body
     });
     thing.save().then(()=> res.status(201).json({message: 'Objet enregistré !'}))
@@ -35,9 +35,20 @@ app.post('/api/stuff', (req, res, next) => {
 app.get('/api/stuff/:id', (req, res, next) =>{
     Thing.findOne({_id: req.params.id})
         .then(thing => res.status(200).json(thing))
-        .catch(error => res.status(400).json({error}))
+        .catch(error => res.status(404).json({error}))
 });
+app.delete('/api/stuff/:id', (req, res, next)=>{
+    Thing.deleteOne({_id: req.params.id})
+        .then(() => res.status(200).json({message:'Objet Supprimer !'}))
+        .catch(error => res.status(404).json({error}))
+})
 
+app.put('/api/stuff/:id', (req, res, next) => {
+
+    Thing.updateOne({_id: req.params.id}, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({message: "L'objet a été modifier"}))
+        .catch(error => res.status(400).json({error}));
+});
 app.get('/api/stuff', (req, res, next) => {
     Thing.find()
         .then(things => res.status(200).json(things))
